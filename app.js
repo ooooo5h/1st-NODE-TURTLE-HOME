7b3d23301bb0268e9f0fa9ee3eb6f034a9149a53
eunhyung <ooooo5h91@gmail.com>
2022-08-22 13:51:10 +0900

    초기세팅

---
+++ app.js
@@ -0,0 +1,19 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+// 미들웨어 등록
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+// 주소 라우팅
+app.get("/", (req, res) => {
+    res.send('노드 테스트')
+})
+
+// 서버 키기
+const port = 4000;
+app.listen(port, ()=> {
+    console.log('4000포트 서버 켜졌습니다')
+})
