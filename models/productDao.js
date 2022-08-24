1e6a00e2dcd3f1be809378ed8a27ce1de0ae4b1d
eunhyung <ooooo5h91@gmail.com>
2022-08-24 16:25:54 +0900

    전체상품 조회 수정(가격 정보 추가)

--- models/productDao.js
+++ models/productDao.js
@@ -3,14 +3,13 @@ const db = require("../config/mysql");
 const getAllProducts = async () => {
     const sql = `
         SELECT 
-            products.id, 
-            products.name, 
-            products.number, 
-            products.description, 
-            products.image_url
+            p.id, p.name, p.image_url, min(price) as 'min_price', max(price) as 'max_price'
         FROM 
-            products
-        `;
+            products_options as po
+        JOIN 
+            products as p on p.id = po.product_id
+        GROUP BY 
+            product_id;`;
     const [rows, fields] = await db.query(sql);
     return rows;
 };
