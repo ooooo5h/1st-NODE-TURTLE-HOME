134f103be4f612e8445ea2c641505a06cbc39fdc
eunhyung <ooooo5h91@gmail.com>
2022-08-31 21:05:46 +0900

    수정 : 상품 전체/정렬/필터 수정

--- models/productDao.js
+++ models/productDao.js
@@ -1,11 +1,220 @@
 const db = require("../utils/database");
 
+// const getAllProducts = async (filterInfo) => {
+//     let sql ;
+//     if (!filterInfo.min_price) {
+//         sql = `
+//         SELECT 
+//             p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+//         FROM 
+//             products AS p
+//         JOIN 
+//             products_options AS options_min
+//                 ON options_min.id = (
+//                     SELECT id
+//                     FROM products_options 
+//                     WHERE products_options.product_id = p.id
+//                     ORDER BY price ASC
+//                     LIMIT 1)
+//         JOIN 
+//             products_options AS options_max
+//                 ON options_max.id = (
+//                     SELECT id
+//                     FROM products_options 
+//                     WHERE products_options.product_id = p.id
+//                     ORDER BY price DESC
+//                     LIMIT 1)
+//         JOIN
+//             products_options AS po ON po.product_id = p.id
+//         WHERE 
+//             po.size_id = ${filterInfo.size}
+//         LIMIT 
+//             ${filterInfo.limit}
+//         OFFSET
+//             ${filterInfo.offset};`
+//     } else {
+//         sql = `
+//         SELECT 
+//             p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+//         FROM 
+//             products AS p
+//         JOIN 
+//             products_options AS options_min
+//                 ON options_min.id = (
+//                     SELECT id
+//                     FROM products_options 
+//                     WHERE products_options.product_id = p.id
+//                     ORDER BY price ASC
+//                     LIMIT 1)
+//         JOIN 
+//             products_options AS options_max
+//                 ON options_max.id = (
+//                     SELECT id
+//                     FROM products_options 
+//                     WHERE products_options.product_id = p.id
+//                     ORDER BY price DESC
+//                     LIMIT 1)
+//         JOIN
+//             products_options AS po ON po.product_id = p.id
+//         WHERE 
+//             options_min.price < ${filterInfo.max_price} AND options_min.price > ${filterInfo.min_price}
+//         AND 
+//             po.size_id = ${filterInfo.size}
+//         LIMIT 
+//             ${filterInfo.limit}
+//         OFFSET
+//             ${filterInfo.offset};`
+//     }
+//     const [rows, ] = await db.query(sql);
+//     return rows;
+// };
+
 const getAllProducts = async (filterInfo) => {
-    let sql ;
-    if (!filterInfo.min_price) {
-        sql = `
+    // LIMIT과 OFFSET만 적용된 코드 , 노정렬, 노필터
+    const sql = `
+    SELECT 
+        p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+    FROM 
+        products AS p
+    JOIN 
+        products_options AS options_min
+            ON options_min.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price ASC
+                LIMIT 1)
+    JOIN 
+        products_options AS options_max
+            ON options_max.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price DESC
+                LIMIT 1)
+    LIMIT 
+        ${filterInfo.limit}
+    OFFSET
+        ${filterInfo.offset};`
+    const [rows, ] = await db.query(sql);
+    return rows;
+};
+
+const getAllProductsWithPriceFilter = async (optionsInfo) => {
+    // 노정렬, 가격 필터만
+    const sql = `
+    SELECT 
+        p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+    FROM 
+        products AS p
+    JOIN 
+        products_options AS options_min
+            ON options_min.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price ASC
+                LIMIT 1)
+    JOIN 
+        products_options AS options_max
+            ON options_max.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price DESC
+                LIMIT 1)
+    WHERE 
+        options_min.price < ${optionsInfo.max_price} AND options_min.price > ${optionsInfo.min_price}
+    LIMIT 
+        ${optionsInfo.limit}
+    OFFSET
+        ${optionsInfo.offset};`
+    const [rows, ] = await db.query(sql);
+    return rows
+}
+
+const getAllProductsWithPriceAndSizeFilter = async (optionsInfo) => {
+    // 노정렬, 가격필터 + 사이즈필터
+    const sql = `
+    SELECT 
+        p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+    FROM 
+        products AS p
+    JOIN 
+        products_options AS options_min
+            ON options_min.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price ASC
+                LIMIT 1)
+    JOIN 
+        products_options AS options_max
+            ON options_max.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price DESC
+                LIMIT 1)
+    JOIN
+        products_options AS po ON po.product_id = p.id
+    WHERE 
+        options_min.price < ${optionsInfo.max_price} AND options_min.price > ${optionsInfo.min_price}
+    AND
+        po.size_id = ${optionsInfo.size}
+    LIMIT 
+        ${optionsInfo.limit}
+    OFFSET
+        ${optionsInfo.offset};`
+    const [rows, ] = await db.query(sql);
+    return rows
+}
+
+const getAllProductsWithSizeFilter = async (optionsInfo) => {
+    // 노정렬, 사이즈필터
+    const sql = `
+    SELECT 
+        p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+    FROM 
+        products AS p
+    JOIN 
+        products_options AS options_min
+            ON options_min.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price ASC
+                LIMIT 1)
+    JOIN 
+        products_options AS options_max
+            ON options_max.id = (
+                SELECT id
+                FROM products_options 
+                WHERE products_options.product_id = p.id
+                ORDER BY price DESC
+                LIMIT 1)
+    JOIN
+        products_options AS po ON po.product_id = p.id
+    WHERE 
+        po.size_id = ${optionsInfo.size}
+    LIMIT 
+        ${optionsInfo.limit}
+    OFFSET
+        ${optionsInfo.offset};`
+    const [rows, ] = await db.query(sql);
+    return rows
+}
+
+const getSortedProductsWithPrice = async (optionsInfo) => {
+    const sortObject = {
+        1: "ORDER BY p.created_at DESC",
+        2: "ORDER BY min_price DESC",
+        3: "ORDER BY min_price ASC",
+    };
+
+    const sql = `
         SELECT 
-            p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+            p.id, p.name, p.image_url, options_min.price as min_price, options_max.price as max_price
         FROM 
             products AS p
         JOIN 
@@ -15,27 +224,38 @@ const getAllProducts = async (filterInfo) => {
                     FROM products_options 
                     WHERE products_options.product_id = p.id
                     ORDER BY price ASC
-                    LIMIT 1)
+                    LIMIT 1
+                )
         JOIN 
             products_options AS options_max
                 ON options_max.id = (
                     SELECT id
                     FROM products_options 
                     WHERE products_options.product_id = p.id
-                    ORDER BY price DESC
-                    LIMIT 1)
-        JOIN
-            products_options AS po ON po.product_id = p.id\
-        AND 
-            po.size_id = ${filterInfo.size}
+                    ORDER BY price desc
+                    LIMIT 1
+                )
+        WHERE 
+            options_min.price < ${optionsInfo.max_price} AND options_min.price > ${optionsInfo.min_price}
+        ${sortObject[`${optionsInfo.sort}`]}
         LIMIT 
-            ${filterInfo.limit}
+            ${optionsInfo.limit}
         OFFSET
-            ${filterInfo.offset};`
-    } else {
-        sql = `
+            ${optionsInfo.offset}`;
+    const [rows, ] = await db.query(sql);
+    return rows;
+};
+
+const getSortedProductsWithPriceAndSizeFilter = async (optionsInfo) => {
+    const sortObject = {
+        1: "ORDER BY p.created_at DESC",
+        2: "ORDER BY min_price DESC",
+        3: "ORDER BY min_price ASC",
+    };
+
+    const sql = `
         SELECT 
-            p.id, p.name, p.image_url, options_min.price AS min_price, options_max.price AS max_price
+            p.id, p.name, p.image_url, options_min.price as min_price, options_max.price as max_price
         FROM 
             products AS p
         JOIN 
@@ -45,26 +265,71 @@ const getAllProducts = async (filterInfo) => {
                     FROM products_options 
                     WHERE products_options.product_id = p.id
                     ORDER BY price ASC
-                    LIMIT 1)
+                    LIMIT 1
+                )
         JOIN 
             products_options AS options_max
                 ON options_max.id = (
                     SELECT id
                     FROM products_options 
                     WHERE products_options.product_id = p.id
-                    ORDER BY price DESC
-                    LIMIT 1)
+                    ORDER BY price desc
+                    LIMIT 1
+                )
         JOIN
             products_options AS po ON po.product_id = p.id
         WHERE 
-            options_min.price < ${filterInfo.max_price} AND options_min.price > ${filterInfo.min_price}
-        AND 
-            po.size_id = ${filterInfo.size}
+            options_min.price < ${optionsInfo.max_price} AND options_min.price > ${optionsInfo.min_price}
+        AND
+            po.size_id = ${optionsInfo.size}
+        ${sortObject[`${optionsInfo.sort}`]}
         LIMIT 
-            ${filterInfo.limit}
+            ${optionsInfo.limit}
         OFFSET
-            ${filterInfo.offset};`
-    }
+            ${optionsInfo.offset}`;
+    const [rows, ] = await db.query(sql);
+    return rows;
+};
+
+const getSortedProductsWithSizeFilter = async (optionsInfo) => {
+    const sortObject = {
+        1: "ORDER BY p.created_at DESC",
+        2: "ORDER BY min_price DESC",
+        3: "ORDER BY min_price ASC",
+    };
+
+    const sql = `
+        SELECT 
+            p.id, p.name, p.image_url, options_min.price as min_price, options_max.price as max_price
+        FROM 
+            products AS p
+        JOIN 
+            products_options AS options_min
+                ON options_min.id = (
+                    SELECT id
+                    FROM products_options 
+                    WHERE products_options.product_id = p.id
+                    ORDER BY price ASC
+                    LIMIT 1
+                )
+        JOIN 
+            products_options AS options_max
+                ON options_max.id = (
+                    SELECT id
+                    FROM products_options 
+                    WHERE products_options.product_id = p.id
+                    ORDER BY price desc
+                    LIMIT 1
+                )
+        JOIN
+            products_options AS po ON po.product_id = p.id
+        WHERE 
+            po.size_id = ${optionsInfo.size}
+        ${sortObject[`${optionsInfo.sort}`]}
+        LIMIT 
+            ${optionsInfo.limit}
+        OFFSET
+            ${optionsInfo.offset}`;
     const [rows, ] = await db.query(sql);
     return rows;
 };
@@ -99,13 +364,6 @@ const getSortedProducts = async (optionsInfo) => {
                     ORDER BY price desc
                     LIMIT 1
                 )
-        JOIN
-			products_options AS po
-				ON po.product_id = p.id
-        WHERE 
-            options_min.price < ${optionsInfo.max_price} AND options_min.price > ${optionsInfo.min_price}
-        AND 
-			po.size_id = ${optionsInfo.size}
         ${sortObject[`${optionsInfo.sort}`]}
         LIMIT 
             ${optionsInfo.limit}
@@ -141,6 +399,12 @@ const getProductById = async (productId) => {
 
 module.exports = {
     getAllProducts,
-    getProductById,
+    getAllProductsWithPriceFilter,
+    getAllProductsWithPriceAndSizeFilter,
+    getAllProductsWithSizeFilter,
+    getSortedProductsWithPrice,
+    getSortedProductsWithPriceAndSizeFilter,
+    getSortedProductsWithSizeFilter,
     getSortedProducts,
+    getProductById,
 };
--- services/productService.js
+++ services/productService.js
@@ -3,9 +3,54 @@ const productDao = require("../models/productDao");
 const getAllProductList = async (optionsInfo) => {
   let result;
   if (optionsInfo.sort == 0) {
-    result = await productDao.getAllProducts(optionsInfo);
+    // ##### step 1-1 :정렬 안한다!  
+    // ==> 가격필터는 min_price & max_price 무조건 양쪽 다 있어야 한다 가정하기
+
+    if (optionsInfo.min_price && (optionsInfo.size == 0)) {
+      // 가격필터 있고 사이즈 없고
+      // 가격 필터 걸어서 전체상품 가져오기
+      console.log('정렬 X 가격필터 O 사이즈 X')
+      result = await productDao.getAllProductsWithPriceFilter(optionsInfo);
+
+    } else if (optionsInfo.min_price && (optionsInfo.size != 0)) {
+      // 가격필터 있고 사이즈 있고
+      // 가격필터 + 사이즈필터 걸어서 전체 상품 가져오기
+      console.log('정렬 X 가격필터 O 사이즈 O')
+      result = await productDao.getAllProductsWithPriceAndSizeFilter(optionsInfo);
+
+    } else if (!optionsInfo.min_price && (optionsInfo.size != 0)) {
+      // 가격필터 없고 사이즈 있고
+      console.log('가격필터 X 사이즈 O')
+      result = await productDao.getAllProductsWithSizeFilter(optionsInfo);
+
+    } else { 
+      // 가격필터 없고 사이즈 없고
+      console.log('정렬 X 가격필터 X 사이즈 X => 그냥 전체 상품 보내주기')
+      result = productDao.getAllProducts(optionsInfo)
+    }
+
   } else {
-    result = await productDao.getSortedProducts(optionsInfo);
+    // ##### step 1-2 :정렬 한다!
+    if (optionsInfo.min_price && (optionsInfo.size == 0)) {
+      // 가격필터 있고 사이즈 없고
+      console.log('정렬 O 가격필터 O 사이즈 X')
+      result = await productDao.getSortedProductsWithPrice(optionsInfo);
+
+    } else if (optionsInfo.min_price && (optionsInfo.size != 0)) {
+      // 가격필터 있고 사이즈 있고
+      console.log('정렬 O 가격필터 O 사이즈 O')
+      result = await productDao.getSortedProductsWithPriceAndSizeFilter(optionsInfo);
+
+    } else if (!optionsInfo.min_price && (optionsInfo.size != 0)) {
+      // 가격필터 없고 사이즈 있고
+      console.log('정렬 O 가격필터 X 사이즈 O')
+      result = await productDao.getSortedProductsWithSizeFilter(optionsInfo);
+
+    } else { 
+      // 가격필터 없고 사이즈 없고
+      console.log('정렬 O 가격필터 X 사이즈 X')
+      result = await productDao.getSortedProducts(optionsInfo);
+    }
   }
   return result;
 };
@@ -22,4 +67,4 @@ const getProductById = async (productId) => {
 module.exports = {
   getAllProductList,
   getProductById,
-};
+}
