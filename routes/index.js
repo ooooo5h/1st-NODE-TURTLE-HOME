0ff576d34051287afe7aaed9ed0766ed9879eba8
eunhyung <ooooo5h91@gmail.com>
2022-08-31 18:07:26 +0900

    수정 : 장바구니 url 주소 수정

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const auth          = require("../middleware/auth");
 
 router.use("/users", userRouter.router);
 router.use("/products", productRouter.router);
-router.use("/cart", auth.verifyAccessToken, cartRouter.router);
+router.use("/carts", auth.verifyAccessToken, cartRouter.router);
 router.use("/auth", authRouter.router);   // step1 : access_token이 만료되면 여기로 오세요 => 헤더에 리프레시 토큰 첨부해주세요~
 
 module.exports = router;
